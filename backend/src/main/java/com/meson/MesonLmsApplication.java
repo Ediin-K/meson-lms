e248f97e550581078556e34ec492d49834bab7c2
Edin <edinkamberi26@gmail.com>
2026-04-04 00:51:28 +0200

    Disa konfigurime ne vite

---
+++ backend/src/main/java/com/meson/MesonLmsApplication.java
@@ -0,0 +1,12 @@
+package com.meson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesonLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesonLmsApplication.class, args);
+    }
+}
