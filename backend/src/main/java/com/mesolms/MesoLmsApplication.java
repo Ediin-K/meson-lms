2d858446e275ec0d794c764fa92fcff4fca1853b
Edin <edinkamberi26@gmail.com>
2026-04-03 23:35:02 +0200

    Disa komponente te nevojitshme per krijimin e lidhjes me databaz MySQL

---
+++ backend/src/main/java/com/mesolms/MesoLmsApplication.java
@@ -0,0 +1,12 @@
+package com.mesolms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesoLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesoLmsApplication.class, args);
+    }
+}
