83f8c8ca7b20c86efd032b09a022bd40b51b0e7a
Edin <edinkamberi26@gmail.com>
2026-04-10 20:25:13 +0200

    Krijohen edhe tabelat tjera, tani eshte gati per te krijuar rest api

---
+++ backend/src/main/entity/UserToken.java
@@ -0,0 +1,31 @@
+package com.meson.mesonlmsbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_tokens")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable=false)
+    private User user;
+
+    @Column(nullable = false)
+    private String loginProvider;
+
+    @Column(nullable = false)
+    private String tokenName;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String tokenValue;
+
+}
