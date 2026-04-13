3a306627163ef2f162422f272adcc7a9983726ac
Edin <edinkamberi26@gmail.com>
2026-04-13 18:02:47 +0200

    login register, jan rregullura plotesisht, useri mund te ndahet ne role, varesisht prej rolit ai sheh login logout, next----> duhet qe secili rol te ket faqet e veta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Header from "./components/ui/Header.jsx";
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import About from './pages/About.jsx';
-import Support from './pages/Support.jsx'
-import ContactUs from './pages/ContactUs.jsx';
 import CourseDetail from './pages/CourseDetail.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'swiper/css';
@@ -21,11 +19,9 @@ function App() {
           tabIndex={-1}
         >
           <Routes>
-            <Route path="/support" element={<Support />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/about" element={<About />} />
-            <Route path="/contactus" element={<ContactUs />} />
             <Route path="/course/:courseId" element={<CourseDetail />} />
             <Route path="/" element={<Home />} />
           </Routes>
